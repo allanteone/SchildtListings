c16137698d5547d5d39ecf40d63f9c1f67256a69
allan <allanteone@gmail.com>
2023-05-18 17:42:11 +0300

    updates LambdaDemo

---
+++ src/main/java/com/AllanTeone/javacore/chapter15/LambdaDemo.java
@@ -0,0 +1,20 @@
+package main.java.com.AllanTeone.javacore.chapter15;
+
+interface MyNumber {
+    double getValue();
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        MyNumber myNum;
+
+        myNum = () -> 123.45;
+
+        System.out.println("Фиксированное значение: " + myNum.getValue());
+
+        myNum = () -> Math.random() * 100;
+
+        System.out.println("Случайное значение: " + myNum.getValue());
+        System.out.println("Ещё одно случайное значение: " + myNum.getValue());
+    }
+}
