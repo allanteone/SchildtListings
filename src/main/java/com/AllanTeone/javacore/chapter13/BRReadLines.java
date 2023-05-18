e8ae2e0b6d5c728171a1e0fbd10a6996565ced3f
allan <allanteone@gmail.com>
2023-05-18 16:14:34 +0300

    adds BRReadLines

---
+++ src/main/java/com/AllanTeone/javacore/chapter13/BRReadLines.java
@@ -0,0 +1,19 @@
+package main.java.com.AllanTeone.javacore.chapter13;
+
+import java.io.*;
+
+
+public class BRReadLines {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        System.out.println("Введите строки текста.");
+        System.out.println("Введите 'стоп' для завершения.");
+
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("стоп"));
+    }
+}
