6844f7563d2a0cc1c57a7ee5d8766fb62a14c4ae
allan <allanteone@gmail.com>
2023-05-18 16:11:38 +0300

    adds BRRead

---
+++ src/main/java/com/AllanTeone/javacore/chapter13/BRRead.java
@@ -0,0 +1,17 @@
+package main.java.com.AllanTeone.javacore.chapter13;
+
+import java.io.*;
+
+
+public class BRRead {
+    public static void main(String[] args) throws IOException {
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите символы, 'q' - для выхода.");
+
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
+    }
+}
