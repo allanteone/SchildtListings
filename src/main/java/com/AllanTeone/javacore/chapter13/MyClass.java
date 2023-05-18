65dc331d5121581a37ce0e2a763e4d5c697f0924
allan <allanteone@gmail.com>
2023-05-18 17:35:47 +0300

    updates MyClass

---
+++ src/main/java/com/AllanTeone/javacore/chapter13/MyClass.java
@@ -0,0 +1,19 @@
+package main.java.com.AllanTeone.javacore.chapter13;
+
+public class MyClass {
+    int a;
+    int b;
+
+    MyClass(int i, int j) {
+        a = i;
+        b = j;
+    }
+
+    MyClass(int i) {
+        this(i, i);
+    }
+
+    MyClass() {
+        this(0);
+    }
+}
