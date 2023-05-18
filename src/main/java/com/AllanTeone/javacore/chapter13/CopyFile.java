9611a97f5b8d21c744607e231c4287f9f0ba4dd2
allan <allanteone@gmail.com>
2023-05-18 16:51:49 +0300

    updates CopyFile

---
+++ src/main/java/com/AllanTeone/javacore/chapter13/CopyFile.java
@@ -0,0 +1,41 @@
+package main.java.com.AllanTeone.javacore.chapter13;
+
+import java.io.*;
+
+
+public class CopyFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        if (args.length != 2) {
+            System.out.println("Использование: CopyFile откуда куда");
+        }
+
+        try {
+            fin = new FileInputStream(args[0]);
+            fout = new FileOutputStream(args[1]);
+
+            do {
+                i = fin.read();
+
+                if (i != -1) fout.write(i);
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода: " + e);
+        } finally {
+            try {
+                if (fin != null) fin.close();
+            } catch (IOException e2) {
+                System.out.println("Ошибка закрытия файла ввода.");
+            }
+
+            try {
+                if (fout != null) fout.close();
+            } catch (IOException e2) {
+                System.out.println("Ошибка закрытия файла вывода.");
+            }
+        }
+    }
+}
