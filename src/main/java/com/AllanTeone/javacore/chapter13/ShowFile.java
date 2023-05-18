70d5796f4e5663f5242b6bdd4e9f79afdb09f4df
allan <allanteone@gmail.com>
2023-05-18 16:36:32 +0300

    updates ShowFile

--- src/main/java/com/AllanTeone/javacore/chapter13/ShowFile.java
+++ src/main/java/com/AllanTeone/javacore/chapter13/ShowFile.java
@@ -5,33 +5,31 @@ import java.io.*;
 public class ShowFile {
     public static void main(String[] args) {
         int i;
-        FileInputStream fin;
+        FileInputStream fin = null;
 
         if (args.length != 1) {
             System.out.println("Использование: ShowFile имя_файла");
             return;
         }
-
+        
         try {
             fin = new FileInputStream(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.println("Невозможно открыть файл");
-            return;
-        }
 
-        try {
             do {
                 i = fin.read();
                 if (i != -1) System.out.print((char) i);
             } while (i != -1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
         } catch (IOException e) {
-            System.out.println("Ошибка чтения из файла");
+            System.out.println("Произошла ошибка ввода-вывода.");
+        } finally {
+            try {
+                if (fin != null) fin.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла.");
+            }
         }
-
-        try {
-            fin.close();
-        } catch (IOException e) {
-            System.out.println("Ошибка закрытия файла");
-        }
+        
     }
 }
