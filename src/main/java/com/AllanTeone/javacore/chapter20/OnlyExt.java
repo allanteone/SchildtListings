6f691499de70e23f32d3cadf55376a5b28350e3a
allan <allanteone@gmail.com>
2023-05-19 13:16:36 +0300

    adds OnlyExt

---
+++ src/main/java/com/AllanTeone/javacore/chapter20/OnlyExt.java
@@ -0,0 +1,16 @@
+package main.java.com.AllanTeone.javacore.chapter20;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class OnlyExt  implements FilenameFilter {
+    String ext;
+
+    public OnlyExt(String ext) {
+        this.ext = "." + ext;
+    }
+
+    public boolean accept(File dir, String name) {
+        return name.endsWith(ext);
+    }
+}
