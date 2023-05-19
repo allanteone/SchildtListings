4f8a70e4e184bd6f5c440d95e941ad4392220b81
allan <allanteone@gmail.com>
2023-05-19 11:12:09 +0300

    updates VarCapture

---
+++ src/main/java/com/AllanTeone/javacore/chapter15/VarCapture.java
@@ -0,0 +1,16 @@
+package main.java.com.AllanTeone.javacore.chapter15;
+
+interface  MyFunc {
+    int func(int n);
+}
+public class VarCapture {
+    public static void main(String[] args) {
+        int num = 10;
+
+        MyFunc myLambda = (n) -> {
+            int v = num + n;
+            return v;
+        };
+        //num = 9;
+    }
+}
