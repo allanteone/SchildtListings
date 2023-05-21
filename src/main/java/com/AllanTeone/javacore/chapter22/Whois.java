f267c64b2e1e8016ff7d308fd6278b74c73f5c8f
allan <allanteone@gmail.com>
2023-05-21 13:27:35 +0300

    update Whois

--- src/main/java/com/AllanTeone/javacore/chapter22/Whois.java
+++ src/main/java/com/AllanTeone/javacore/chapter22/Whois.java
@@ -6,23 +6,23 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class Whois {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         int c;
 
-        Socket s = new Socket("whois.internic.net", 43);
+        try (Socket s = new Socket("whois.internic.net", 43))
+        {
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
 
-        InputStream in = s.getInputStream();
-        OutputStream out = s.getOutputStream();
+            String str = (args.length == 0 ? "MHProfessional.com" : args[0]) + "\n";
 
-        String str = (args.length == 0 ? "MHProfessional.com" : args[0]) + "\n";
+            byte buf[] = str.getBytes();
 
-        byte buf[] = str.getBytes();
+            out.write(buf);
 
-        out.write(buf);
-
-        while ((c = in.read()) != -1) {
-            System.out.print((char) c);
+            while ((c = in.read()) != -1) {
+                System.out.print((char) c);
+            }
         }
-        s.close();
     }
 }
